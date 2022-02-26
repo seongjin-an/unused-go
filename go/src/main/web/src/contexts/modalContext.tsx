d33170ac2b9690e438e9c203d7ec80645c28b150
seongjin-an <seongjin4us@gmail.com>
2022-02-27 01:44:51 +0900

    add context

---
+++ go/src/main/web/src/contexts/modalContext.tsx
@@ -0,0 +1,37 @@
+import React, {createContext, useState} from 'react'
+
+interface IState{
+    isOpen: boolean;
+}
+
+interface IAction{
+    setIsOpen: (prev: boolean) => void;
+}
+
+interface IContext{
+    state: IState;
+    action: IAction
+}
+
+const ModalContext: React.Context<IContext> = createContext<IContext>({
+    state: {isOpen: false},
+    action: {
+        setIsOpen: (prev: boolean) => {}
+    }
+
+})
+
+const ModalContextProvider: React.FC = ({children}) => {
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+    const value = {
+        state: {isOpen},
+        action: {setIsOpen}
+    }
+    return(
+        <ModalContext.Provider value={value}>{children}</ModalContext.Provider>
+    )
+}
+
+const {Consumer: ModalContextConsumer} = ModalContext
+export {ModalContextProvider, ModalContextConsumer}
+export default ModalContext
