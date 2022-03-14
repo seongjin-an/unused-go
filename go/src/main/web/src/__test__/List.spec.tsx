209251e477c0001a083459b3fe76326786d86c87
seongjin-an <seongjin4us@gmail.com>
2022-03-14 23:57:11 +0900

    react-query placeholder and jest!

--- go/src/main/web/src/__test__/List.spec.tsx
+++ go/src/main/web/src/__test__/List.spec.tsx
@@ -4,8 +4,11 @@ import {List} from "../components/molecules/contents/list";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import {QueryClient, QueryClientProvider} from "react-query";
 import {createFakeProducts2} from "../mocks/createFakes";
+import {BrowserRouter, Routes} from "react-router-dom";
+import {RecoilRoot} from "recoil";
+import {ModalContextProvider} from "../contexts/modalContext";
 
-const setup = <TOriginalProps extends {}>(WrappedComponent: React.FunctionComponent<TOriginalProps>, props: TOriginalProps) => {
+const setup = <TOriginalProps extends {}>(WrappedComponent: React.FunctionComponent<TOriginalProps>, props?: TOriginalProps) => {
     const queryClient = new QueryClient({
         defaultOptions: {
             queries: {
@@ -16,16 +19,18 @@ const setup = <TOriginalProps extends {}>(WrappedComponent: React.FunctionCompon
     })
     const wrapper = mount(
         <QueryClientProvider client={queryClient}>
-            <WrappedComponent {...props} />
+            <WrappedComponent {...props!} />
         </QueryClientProvider>
     )
     return {
         wrapper
     }
 }
-interface IListProps{
+
+interface IListProps {
     listType: 'product' | 'community'
 }
+
 describe("list test", () => {
 
     beforeEach(() => {
@@ -37,14 +42,21 @@ describe("list test", () => {
         expect(component).toMatchSnapshot()
     })
 
-    it('should render exactly', async () => {
+    it('should render exactly', () => {
         const {wrapper} = setup<IListProps>(List, {listType: 'product'})
+        console.log('wrapper:', wrapper)
         expect(wrapper.find('ListItem')).toHaveLength(10)
         const listItems = wrapper.find('ListItem')
         listItems.forEach((item, index) => {
             // expect(item.text().includes('아령')).toBe(true)
             expect(item.text()).toContain('아령')
+            expect(item.text()).toContain('10,000원')
         })
         expect(listItems.get(0)).toHaveProperty('key', "0")
+
+        // expect(listItems.at(0).find('ListItemCountValue').text()).toContain("관심")
+        expect(wrapper.find('ListItemCountValue').at(0).text()).toContain("관심")
+
+
     })
 })
