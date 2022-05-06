0d8407dad16a817edd452925d8a92fed41f0fa87
seongjin-an <seongjin4us@gmail.com>
2022-05-07 01:02:19 +0900

    add security configuration

---
+++ go/src/main/kotlin/com/unused/go/configuration/security/SecurityUtil.kt
@@ -0,0 +1,17 @@
+package com.unused.go.configuration.security
+
+import org.springframework.security.core.context.SecurityContextHolder
+
+class SecurityUtil {
+    companion object {
+        // SecurityContext 에 유저 정보가 저장되는 시점
+        // Request 가 들어올 때 JwtFilter 의 doFilter 에서 저장
+        fun getCurrentMemberId(): String{
+            val authentication = SecurityContextHolder.getContext().authentication
+            if(authentication == null || authentication.name == null){
+                throw RuntimeException("Security context 에 인증 정보가 없습니다.")
+            }
+            return authentication.name
+        }
+    }
+}
