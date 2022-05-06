b7979fa2f3d4b594dc5e49ac583cf53ce50f0d85
seongjin-an <seongjin4us@gmail.com>
2022-05-07 01:03:21 +0900

    add enableJpaAuditing annotation

--- go/src/main/kotlin/com/unused/go/GoApplication.kt
+++ go/src/main/kotlin/com/unused/go/GoApplication.kt
@@ -3,9 +3,11 @@ package com.unused.go
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableJpaAuditing
 class GoApplication
 
 fun main(args: Array<String>) {
