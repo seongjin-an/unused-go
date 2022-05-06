0957a68ecffa9d2edf08fb3f10759862cdb18fd9
seongjin-an <seongjin4us@gmail.com>
2022-05-07 01:01:03 +0900

    add dependency of jwt

--- go/build.gradle.kts
+++ go/build.gradle.kts
@@ -38,6 +38,12 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    testImplementation("org.springframework.security:spring-security-test")
+//    implementation("io.jsonwebtoken:jjwt:0.9.1")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.2")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
 }
 val webappDir = "$projectDir/src/main/web"
 
